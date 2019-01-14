4098fd06e736398b808a3c8f1fda0a0bbacea977
nightstorm0909 <nilotpalsinha1990@gmail.com>
2019-01-14 20:31:11 +0800

    MCTS weights modified

--- DLplay_keras_MCTS.py
+++ DLplay_keras_MCTS.py
@@ -180,7 +180,7 @@ loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
 # load weights into new model
-model.load_weights(os.path.join("nets","keras_model_weights_516719_100e.h5"))
+model.load_weights(os.path.join("nets","keras_model_weights_862502_100e.h5"))
 player = ChessPlayer(model, s)
 
 def to_svg(s):
